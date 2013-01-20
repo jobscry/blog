5e83c058e914df9c8138e367cd9588dee7d8e7d9
Joe Vasquez <joe.vasquez@gmail.com>
2013-01-20 07:43:34 -0600

    added mezzanine management commands

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -194,7 +194,7 @@ if DEBUG:
     INSTALLED_APPS += ('debug_toolbar',)
     INTERNAL_IPS = ('127.0.0.1',)
 
-try:
-    from live_settings import *
-except ImportError:
-    pass
+#try:
+#    from live_settings import *
+#except ImportError:
+#    pass
