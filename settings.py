e521d45c30dc4205024d5df5d25b7779b6f20dba
Joe Vasquez <joe.vasquez@gmail.com>
2013-09-09 17:54:13 -0500

    added br to allowed tags

--- settings.py
+++ settings.py
@@ -181,7 +181,7 @@ ALLOWED_HOSTS = (
     'imjoevasquez.com'
 )
 
-BLEACH_ALLOWED_TAGS = ['p', 'b', 'i', 'u', 'em', 'strong', 'a', 'ul', 'ol', 'dd', 'dt', 'dl', 'li', 'code', 'blockquote', 'abbr', 'img', 'table', 'tr', 'td', 'th']
+BLEACH_ALLOWED_TAGS = ['p', 'b', 'i', 'u', 'em', 'strong', 'a', 'ul', 'ol', 'dd', 'dt', 'dl', 'li', 'code', 'blockquote', 'abbr', 'img', 'table', 'tr', 'td', 'th', 'br']
 BLEACH_ALLOWED_ATTRIBUTES = ['href', 'title', 'src', 'class', 'height', 'width']
 BLEACH_ALLOWED_STYLES = []
 BLEACH_STRIP_TAGS = True
